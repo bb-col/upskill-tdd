f647479027b5d1c90e1ba0a1b26f1c3fb2165185
Bora Buyukoz <bora91427@gmail.com>
2022-06-08 15:30:56 +0300

    Initial commit

---
+++ src/main/java/com/demo/upskilltdd/UpskillTddApplication.java
@@ -0,0 +1,13 @@
+package com.demo.upskilltdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpskillTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpskillTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/upskilltdd/UpskillTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.upskilltdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpskillTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
